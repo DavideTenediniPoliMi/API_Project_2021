2a3d4c8710df13c9e30c5aef2177cef356bb1cea
DavideTenedini <47054363+DavideTenedini@users.noreply.github.com>
2021-07-15 14:41:24 +0200

    Added fast input for pos int and char, Tested and Submitted

--- codebase.c
+++ codebase.c
@@ -1,7 +1,68 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <ctype.h>
+
+#define MAX_DIG 32
+
+// fast INput-OUTput
+void ingore_garbage_int();
+void ingore_garbage_char();
+int  expect_pos_int();
+char expect_char();
 
 int main() {
-    
+
     return 0;
+}
+
+// fast INput-OUTput
+
+void ingore_garbage_int() {
+    char ch;
+
+    while(!isdigit(ch = getchar())) {}
+
+    ungetc(ch, stdin);
+}
+
+void ingore_garbage_char() {
+    char ch;
+
+    while(!isalpha(ch = getchar())) {}
+
+    ungetc(ch, stdin);
+}
+
+int expect_pos_int() {
+    char digits[MAX_DIG], ch;
+    int i, num, pow10;
+
+    ingore_garbage_int();
+    
+    i = 0;
+    while(isdigit(ch = getchar()) && i < MAX_DIG) {
+        digits[i] = ch;
+        i++;
+    }
+    ungetc(ch, stdin);
+
+    if(i == 0) {
+        return -1;
+    }
+
+    num = 0;
+    pow10 = 1;
+    i--;
+
+    for(; i >= 0 ; i-- , pow10 *= 10) {
+        num += ((digits[i] - '0') * pow10);
+    }
+
+    return num;
+}
+
+char expect_char() {
+    ingore_garbage_char();
+
+    return getchar();
 }
