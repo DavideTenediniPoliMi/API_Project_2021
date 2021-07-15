eceba10853f850dd2060cd864bfbda2e4dce319c
DavideTenedini <47054363+DavideTenedini@users.noreply.github.com>
2021-07-15 16:57:53 +0200

    Improved fast input Final

--- codebase.c
+++ codebase.c
@@ -12,11 +12,37 @@
 # define pc putchar_unlocked
 #endif
 
-// fast Input-Output
-char ingore_garbage_int();
-char ingore_garbage_char();
-int  expect_pos_int();
-int expect_char();
+// fast IO
+int expect_pos_int() {
+    char ch;
+    int num;
+
+    do {
+        ch = gc();
+    } while (ch != EOF && !isdigit(ch));
+
+    if(ch == EOF) {
+        return -1;
+    }
+
+    num = 0;
+    do {
+        num = num * 10 + (ch - '0');
+        ch = gc();
+    } while(isdigit(ch));
+
+    return num;
+}
+
+int expect_char() {
+    char ch;
+
+    do {
+        ch = gc();
+    } while(ch != EOF && !isalpha(ch));
+
+    return (ch == EOF) ? -1 : ch;
+}
 
 int main() {
     int a,b;
@@ -27,50 +53,4 @@ int main() {
     printf("%d", a+b);
         
     return 0;
-}
-
-// fast INput-OUTput
-
-char ingore_garbage_int() {
-    char ch;
-
-    do {
-        ch = gc();
-    } while (ch != EOF && !isdigit(ch));
-    
-    return ch;
-}
-
-char ingore_garbage_char() {
-    char ch;
-
-    do {
-        ch = gc();
-    } while(ch != EOF && !isalpha(ch));
-
-    return ch;
-}
-
-int expect_pos_int() {
-    char ch;
-    int num;
-
-    ch = ingore_garbage_int();
-
-    num = 0;
-    do {
-        num = num * 10 + (ch - '0');
-        ch = gc();
-    } while(isdigit(ch));
-    ungetc(ch, stdin);
-
-    return num;
-}
-
-int expect_char() {
-    char ch;
-
-    ch = ingore_garbage_char();
-
-    return (ch == EOF) ? -1 : ch;
 }
